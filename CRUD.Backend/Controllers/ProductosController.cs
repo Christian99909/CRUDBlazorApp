f28c3ab7cb00b3c83ba612a799b6b048c100a9f5
Christian99909 <christian99909@gmail.com>
2025-05-24 02:08:10 -0600

    Eliminar WeatherForecastController y añadir ProductosController
    Se eliminaron las clases y métodos del archivo `WeatherForecastController.cs`, se introdujo un nuevo controlador `ProductosController.cs` para manejar operaciones de productos, incluyendo la obtención y adición de productos a la base de datos. También se añadieron nuevas dependencias y se configuró el contexto de datos.

---
+++ CRUD.Backend/Controllers/ProductosController.cs
@@ -0,0 +1,69 @@
+﻿using CRUD.Backend.Data;
+using CRUD.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+
+        public ProductosController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Productos.ToListAsync());
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Producto producto)
+        {
+            try
+            {
+
+                _context.Productos.Add(producto);
+                await _context.SaveChangesAsync();
+                return Ok(producto);
+
+
+            }
+            catch (DbUpdateException e)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException!.Message);
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+
+
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return Ok(producto);
+                   
+        
+        }
+
+    }
+}
